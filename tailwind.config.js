378caf0841073a18d0927c4afa9a395e9feaa4a4
atulmishra84 <120441841+atulmishra84@users.noreply.github.com>
2025-08-31 20:16:20 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports={
   content:["./app/**/*.{ts,tsx}","./components/**/*.{ts,tsx}"],
-  theme:{extend:{colors:{brand:{bg:"#0b1220",surface:"#0e1729",cyan:"#27e5ff",cyanSoft:"#1ad3ea",accent:"#8bc6ff"}},boxShadow:{glow:"0 10px 30px rgba(39,229,255,0.25)"},borderRadius:{xl2:"1.25rem"}}},
+  theme:{extend: {
+  colors: {
+    brand: {
+      bg: "#0b1220",
+      surface: "#0e1729",
+      cyan: "#27e5ff",
+      cyanSoft: "#1ad3ea",
+      accent: "#8bc6ff"
+    }
+  }
+}
   plugins:[]
 };
